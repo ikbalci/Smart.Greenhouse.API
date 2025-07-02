f31751f0408c3f4ce916de2ab51a818c6a20d62b
İbrahim Kenan Balcı <i.kenanbalci@gmail.com>
2025-07-02 09:51:25 +0300

    Remove range

--- Smart.Greenhouse.API/Core/DTOs/SensorDataDto.cs
+++ Smart.Greenhouse.API/Core/DTOs/SensorDataDto.cs
@@ -7,10 +7,8 @@ namespace Smart.Greenhouse.API.Core.DTOs
         public string? Date { get; set; }
         public string? Time { get; set; }
         
-        [Range(0, 100)]
         public double Moisture { get; set; }
         
-        [Range(-50, 100)]
         public double Temperature { get; set; }
 
         public double AirQuality {get; set; }
